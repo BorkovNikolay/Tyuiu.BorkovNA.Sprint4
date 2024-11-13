65788b853b814b61d42a47c956468a66b6856916
Nikolay Borkov <ghost0dragon13@gmail.com>
2024-11-13 06:32:57 +0500

    4

---
+++ Tyuiu.BorkovNA.Sprint4.Task4.V21.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Tyuiu.BorkovNA.Sprint4.Task4.V21.Lib;
+
+namespace Tyuiu.BorkovNA.Sprint4.Task4.V21.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new();
+            int[,] input = { { 3, 6, 4, 6, 6 }, { 7, 5, 5, 5, 8 }, { 8, 3, 6, 7, 7 }, { 5, 3, 8, 4, 5 }, { 3, 7, 4, 3, 8 } };
+            Assert.AreEqual(68, ds.Calculate(input));
+        }
+    }
+}
---
+++ Tyuiu.BorkovNA.Sprint4.Task4.V21/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Tyuiu.BorkovNA.Sprint4.Task4.V21.Lib;
+
+
+
+
+namespace Tyuiu.BorkovNA.Sprint4.Task4.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+
+            Console.Title = "Cпринт #4 | Выполнил: Борков Н. А. | пктб-24-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                *");
+            Console.WriteLine("* Тема: Операции сравнения                                                 *");
+            Console.WriteLine("* Задание #4                                                               *");
+            Console.WriteLine("* Вариант #21                                                              *");
+            Console.WriteLine("* Выполнил: Борков Н. А. | пктб-24-1                                       *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.Write("*  = ");
+            int[,] input = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++) { input[i, j] = Convert.ToInt32(Console.ReadLine()); }
+            }
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            int res = ds.Calculate(input);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+
+}
